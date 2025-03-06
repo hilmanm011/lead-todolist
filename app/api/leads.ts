65dee14836b153ea7fc3881e945151009c7aecba
hilmanm011 <muhamadhilman885@gmail.com>
2025-03-07 01:29:16 +0700

    fix base API URL

--- app/api/leads.ts
+++ app/api/leads.ts
@@ -8,7 +8,7 @@ export const fetchLeads = async ({ status = "", skip = 0, limit = 5 }) => {
         limit: limit.toString(),
     }).toString();
 
-    const response = await fetch(`${API_URL}/api/leads?${queryParams}`);
+    const response = await fetch(`${API_URL}/leads?${queryParams}`);
     const responseData = await response.json();
     if (!response.ok) throw new Error(responseData.message || "Failed to fetch leads");
     return responseData.data
@@ -16,7 +16,7 @@ export const fetchLeads = async ({ status = "", skip = 0, limit = 5 }) => {
 
 // Add New Lead
 export const addLead = async (leadData: { name: string; email: string; status: string }) => {
-    const response = await fetch(`${API_URL}/api/leads`, {
+    const response = await fetch(`${API_URL}/leads`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(leadData),
@@ -28,7 +28,7 @@ export const addLead = async (leadData: { name: string; email: string; status: s
 
 // Update Lead
 export const updateLead = async (id: string, leadData: { name: string; email: string; status: string }) => {
-    const response = await fetch(`${API_URL}/api/leads/${id}`, {
+    const response = await fetch(`${API_URL}/leads/${id}`, {
         method: "PUT",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(leadData),
